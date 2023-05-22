13626ff1109d55f9ada3d97302df70bd8e547720
25kjones <120593929+26ASmith@users.noreply.github.com>
2023-05-22 13:07:29 -0400

    wee

--- game.py
+++ game.py
@@ -59,12 +59,9 @@ map6 = Scene(window, link, "ZeldaMapTilesBrown.png", "map6.txt", 6,8)
 map7 = Scene(window, link, "ZeldaMapTilesBrown.png", "map7.txt", 6,8)
 map8 = Scene(window, link, "ZeldaMapTilesBrown.png", "map8.txt", 6,8)
 map9 = Scene(window, link, "ZeldaMapTilesBrown.png", "map9.txt", 6,8)
-<<<<<<< HEAD
 map8 = Scene(window, link, "ZeldaMapTilesBrown.png", "map8.txt", 6,8)
 
 
-=======
->>>>>>> e713e9609850a4cc89882e975a79a22163c219d0
 
 low = [map7, map8, map9]
 middle = [map3, map1, map2]
