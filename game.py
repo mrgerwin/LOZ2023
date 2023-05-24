5d5624ff1fdf325a4524a80a0c7c7dbe5a33cb14
22nsnyder <25dstuder@otsegoknights.org>
2023-05-24 13:20:53 -0400

    Update game.py

--- game.py
+++ game.py
@@ -400,6 +400,21 @@ while True:
                 elif link.orientation == 3:
                     link.rect.x += link.speed
                     link.rect.x +=7
+        for tile in currentScene.Water_Tiles:
+            if touching(link, tile):
+                link.speed=0
+                if link.orientation == 0:
+                    link.rect.y -= link.speed
+                    link.rect.y -=7
+                elif link.orientation == 1:
+                    link.rect.y += link.speed
+                    link.rect.y +=7
+                elif link.orientation == 2:
+                    link.rect.x -= link.speed
+                    link.rect.x -= 7
+                elif link.orientation == 3:
+                    link.rect.x += link.speed
+                    link.rect.x +=7
         if link.rect.x > screenX:
             sceneChange("right")
             link.rect.x = 1
